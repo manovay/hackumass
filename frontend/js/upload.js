a65359bb25cdc6225bd6c417f71a1d29d7e91f79
MANOVAY SHARMA <manovays2004@gmail.com>
2024-11-09 00:20:34 -0500

    added functionality for js for upload

--- frontend/js/upload.js
+++ frontend/js/upload.js
@@ -2,6 +2,26 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const fileInput = document.getElementById("fileInput");
     const message = document.getElementById("message");
 
-    fileInput.addEventListener("submit")
-}
-)
+    fileInput.addEventListener("submit", async(event) =>{
+        const input = document.getElementById("input")
+        const data = new FormData()
+
+        data.append("file",fileInput.files[0]);
+        
+        try{
+            const response = await("/api/upload", {method:"POST", body:data });
+            if (response.ok){
+                message.textContent ="File uploaded";
+                navigateTo(result)
+            }else{
+                message.textContent = "Error uploading"
+            }
+        }
+        catch(error){
+            console.error("error: ", error);
+        }
+    })
+})
+function navigateTo(page) {
+    window.location.href = `${page}.html`;
+  }
